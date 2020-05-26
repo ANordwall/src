6fd662fd5e10b1bfcb93e90f028c9b191dd4b9a4
ANordwall <42980940+ANordwall@users.noreply.github.com>
2020-05-26 11:51:37 -0400

    Add files via upload

--- game.c
+++ game.c
@@ -1,20 +1,15 @@
 #include "elecanisms.h"
 #include "millis.h"
 
-#define p1  D0
-#define l1  D1
-#define start   D7
-#define led D9
-#define p2  D12
-#define l2  D13
-
-int statep1 = 0;
-int statep2 = 0;
-int laststatep1 = 0;
-int laststatep2 = 0;
+#define p1  D0			// Player 1 button
+#define l1  D1			// Player 1 LED
+#define start   D7		// Start button
+#define led D9			// main LED
+#define p2  D12			// Player 2 button
+#define l2  D13			// Player 2 LED
 
 
-// generate a random number between 1 and 5
+// generate a random number between 6000 and 2000
 int random(void) { 
     int num; 
     num = (rand() %6000) +2000; 
@@ -29,40 +24,37 @@ int16_t main(void) {
     init_elecanisms();
     init_millis();
 
-    t0 = millis();
+    t0 = millis();                          //start time
     
-    int r = random();
+    int r = random();                       //random delay length 
     
     while (1) {
 	    t = millis();
-        if (start==0 && t>=t0+r) {
+        if (start==0 && t>=t0+r) {          // if start button is pressed and random time has elapsed
             led = ON;
             while (1){
                 int t1=millis();
-	            if (p1==0){
+	            if (p1==0){                 // if player 1 hits their button
                     led=OFF;
                     l1=ON;
                     while (1){
                         t=millis();
-                        if (t>=t1+3000){
+                        if (t>=t1+2000){    //turn on P1 LED for 2 seconds to indicate win
                             l1=OFF;
-                            main();
+                            main();         //repeat main function
                         }
                     }
                 }
-	        	else if (p2==0){
+	        	else if (p2==0){            //if player 2 hits their button
                     led=OFF;
                     l2=ON;
                     while (1){
                         t=millis();
-                        if (t>=t1+3000){
+                        if (t>=t1+2000){    //turn on P1 LED for 2 seconds to indicate win
                             l2=OFF;
-                            main();
+                            main();         //repeat main function
                         }
                     }
-
-		            //l1 = (p1 == 0) ? ON : OFF; 
-		            //l2 = (p2 == 0) ? ON : OFF; 
 	        	}
 	        }
         }
